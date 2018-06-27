b8b9376256d467ca0897c9ec21a56f0a49a2c97e
Barel, Avi <Avi.Barel@ladbrokescoral.com>
2018-06-27 22:23:52 +0300

    Moved Key to Build Config

--- MyPhotos/app/src/main/java/com/avi/myphotos/network/UrlFactory.kt
+++ MyPhotos/app/src/main/java/com/avi/myphotos/network/UrlFactory.kt
@@ -1,11 +1,13 @@
 package com.avi.myphotos.network
 
+import com.avi.myphotos.BuildConfig
+
 object UrlFactory {
 
     const val PHOTOS_BASE_URL = "https://www.googleapis.com/"
 
     fun getSearchUrl(pageSize: Int, currentPage: Int, query: String): String {
-        return "customsearch/v1?key=AIzaSyCzREl07Bc_bk2hm65RtiaJ0hJ48R_nMfg&cx=005800383728131713214:5jocmduwqum&searchType=image&num=$pageSize&start=$currentPage&q=$query"
+        return "customsearch/v1?key=${BuildConfig.PHOTOS_KEY}&cx=005800383728131713214:5jocmduwqum&searchType=image&num=$pageSize&start=$currentPage&q=$query"
     }
 
 }
